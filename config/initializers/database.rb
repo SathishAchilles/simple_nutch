0ed09a7dcaa4e12737f39a3a0aebdb206591dab8
Sathish <reachtosathish.b@gmail.com>
2020-12-23 22:10:39 +0530

    load db config from yaml

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -1,12 +1,11 @@
 require 'sqlite3'
 require 'active_record'
 require 'logger'
+require 'yaml'
+require 'bundler'
+
+db_config = YAML.load_file("#{Bundler.root}/config/database.yaml")
+env = ENV['SIMPLE_NUTCH_ENV'] || 'development'
 
 ActiveRecord::Base.logger = Logger.new($stdout)
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  dbfile: ':memory:',
-  pool: 5,
-  timeout: 5000,
-  database: 'simple_nudge_dev'
-)
+ActiveRecord::Base.establish_connection(db_config[env])
