e2733f17427439d5dde84e50eec4bf93d24a30d3
Sathish <reachtosathish.b@gmail.com>
2020-12-23 21:17:51 +0530

    shovel query params for sitemap

--- app/modals/site_map.rb
+++ app/modals/site_map.rb
@@ -1,4 +1,16 @@
+# Sitemap modal to store urls & query params
 class SiteMap < ApplicationRecord
   belongs_to :nutch_request
 
+  private
+
+  def shovel_query_params_from_url
+    uri = Addressable::URI.parse(url)
+    query_params = uri.query_values
+    if query_params
+      self.query_params = query_params.keys.join(',')
+      uri.query_values = nil
+    end
+    self.url = uri.to_s
+  end
 end
