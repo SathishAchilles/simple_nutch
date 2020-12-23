6005d3bf182ecf3137e89a718172c4cb3570a056
Sathish <reachtosathish.b@gmail.com>
2020-12-24 04:33:49 +0530

    Use PG over Sqlite3
    Sqlite3 does not allow concurrent writes

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -1,4 +1,4 @@
-require 'sqlite3'
+require 'pg'
 require 'active_record'
 require 'logger'
 require 'yaml'
@@ -8,4 +8,5 @@ db_config = YAML.load_file("#{Bundler.root}/config/database.yaml")
 env = ENV['SIMPLE_NUTCH_ENV'] || 'development'
 
 ActiveRecord::Base.logger = Logger.new($stdout)
+# ActiveRecord::Base.logger.level = Logger::WARN
 ActiveRecord::Base.establish_connection(db_config[env])
