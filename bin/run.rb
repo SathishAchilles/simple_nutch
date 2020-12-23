7c30153c632f499929c32079b1f3203132a60eee
Sathish <reachtosathish.b@gmail.com>
2020-12-23 21:47:44 +0530

    define executable to run

---
+++ bin/run.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# executable to run web server log analyzer
+require 'bundler'
+Dir[File.join(Bundler.root, 'config', 'initializers', '*.rb')].sort.each do |file|
+  require file
+end
+
+if $PROGRAM_NAME == __FILE__
+  crawl_url = ARGV[0]
+  RecursiveCrawler.call(crawl_url)
+end
