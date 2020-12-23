4290fa3ea08ca4df7f1212b095c68f1e1e73d16a
Sathish <reachtosathish.b@gmail.com>
2020-12-23 21:02:34 +0530

    define trackable status & modularize

---
+++ app/modals/status_trackable.rb
@@ -0,0 +1,12 @@
+# Models that needs to track status can include this module
+module StatusTrackable
+  def track_status(raise_exception: false)
+    update(status: :in_progress)
+    block_return_value = yield
+    update(status: :completed)
+    block_return_value
+  rescue StandardError => exception
+    update(status: :failed)
+    log_exception(exception, raise_exception)
+  end
+end
