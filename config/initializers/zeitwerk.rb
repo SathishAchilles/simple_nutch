95891e4ac8019c0717caa96892cbd1a801923cfe
Sathish <reachtosathish.b@gmail.com>
2020-12-23 21:19:09 +0530

    autoload all files from app & lib path

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -4,9 +4,13 @@ require 'zeitwerk'
 require 'bundler'
 
 APP_PATH = Bundler.root.join('app')
+LIB_PATH = Bundler.root.join('lib')
 
 loader = Zeitwerk::Loader.new
 loader.push_dir(APP_PATH)
-loader.push_dir(APP_PATH.join('services'))
+Dir[File.join(APP_PATH, '*')].sort.each do |dir|
+  loader.push_dir(dir)
+end
+loader.push_dir(LIB_PATH)
 loader.setup
 loader.eager_load
