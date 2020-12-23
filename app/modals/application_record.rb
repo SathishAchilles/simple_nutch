4cc1b673d4f16ec66aa89a12b455db09173f760b
Sathish <reachtosathish.b@gmail.com>
2020-12-23 20:58:16 +0530

    define log_exception

--- app/modals/application_record.rb
+++ app/modals/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def log_exception(exception, raise_exception: false)
+    logger.fatal(exception.backtrace)
+    raise_exception && raise
+  end
 end
