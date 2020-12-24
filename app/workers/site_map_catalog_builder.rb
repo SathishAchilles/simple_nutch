85729200aa29f6efd7bdce3d3ea8ce0e7901648f
Sathish <reachtosathish.b@gmail.com>
2020-12-24 06:38:22 +0530

    harvest all links from the given url & refactor

--- app/workers/site_map_catalog_builder.rb
+++ app/workers/site_map_catalog_builder.rb
@@ -7,17 +7,31 @@ class SiteMapCatalogBuilder
   end
 
   def perform(job_id)
-    job = JobQueue.find_by(id: job_id)
-    site_map = SiteMap.find_by_request_id_and_raw_url(job.nutch_request_id, job.url)
+    @job = JobQueue.find_by(id: job_id)
+    @site_map = SiteMap.find_by_request_id_and_raw_url(job.nutch_request_id, job.url)
+    create_or_increment_site_maps
+    harvest_hyperlinks_from_current_url
+  end
+
+  private
+
+  attr_reader :job, :site_map, :duplicates
+
+  def create_or_increment_site_maps
     duplicates = job.duplicate_reference.zero? ? 1 : job.duplicate_reference
     if site_map
       site_map.increment!(:reference_count, duplicates)
     else
-      SiteMap.create(nutch_request_id: job.nutch_request_id, url: job.url, reference_count: duplicates)
+      SiteMap.create(nutch_request: job.nutch_request, url: job.url, reference_count: duplicates)
     end
+  end
 
-    job.track_status do
-      SimpleHTMLHREFParser.call(SimpleCurl.call(job.url), job)
+  def harvest_hyperlinks_from_current_url
+    job.track_status(raise_exception: true) do
+      links = SimpleHTMLHREFParser.call(SimpleCurl.call(job.url))
+      return unless links
+
+      links.each { |link| JobQueueService.call(link, job.nutch_request_id, job.depth + 1) }
     end
   end
 end
