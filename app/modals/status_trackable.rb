f6e0698b7c93658cd03f8b155e5f49c7daf769bf
Sathish <reachtosathish.b@gmail.com>
2020-12-24 06:33:50 +0530

    remove return values while tracking status

--- app/modals/status_trackable.rb
+++ app/modals/status_trackable.rb
@@ -2,9 +2,8 @@
 module StatusTrackable
   def track_status(raise_exception: false)
     update(status: :in_progress)
-    block_return_value = yield
+    yield
     update(status: :completed)
-    block_return_value
   rescue StandardError => exception
     update(status: :failed)
     log_exception(exception, raise_exception: raise_exception)
