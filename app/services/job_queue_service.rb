2545996f802dea508a78ba97ccd9de8ca55a621a
Sathish <reachtosathish.b@gmail.com>
2020-12-24 09:17:28 +0530

    find eligibility to harvest a url and refactor methods

--- app/services/job_queue_service.rb
+++ app/services/job_queue_service.rb
@@ -1,15 +1,17 @@
 # Creates JobQueue only for unique URLs and increments counter for duplicate ref
 class JobQueueService < ApplicationService
+  INCLUDE_NON_MATCHING_DOMAINS = false
   def initialize(url, nutch_request_id, depth)
     @url = url
-    @nutch_request_id = nutch_request_id
+    @nutch_request = NutchRequest.find(nutch_request_id)
     @depth = depth
-    @job_queue = JobQueue.find_by(nutch_request_id: nutch_request_id, url: url)
+    @job_queue = JobQueue.find_by(nutch_request: nutch_request, url: url)
   end
 
   def execute
     if URLValidator.url?(url)
-      create_or_increment_job
+      increment_job_queue if job_queue
+      create_job
     else
       logger.info("Skipping possible static URL #{url}")
       nil
@@ -18,15 +20,25 @@ class JobQueueService < ApplicationService
 
   private
 
-  attr_reader :url, :nutch_request_id, :job_queue, :depth
+  attr_reader :url, :nutch_request, :job_queue, :depth
 
-  def create_or_increment_job
-    if job_queue
-      job_queue&.increment!(:duplicate_reference)
-      logger.info("Incrementing duplicate reference for URL: #{url}")
-    else
-      JobQueue.create(nutch_request_id: nutch_request_id, url: url, depth: depth)
+  def create_job
+    if eligible_to_harvest?(url)
+      JobQueue.create(nutch_request: nutch_request, url: url, depth: depth)
       logger.info("Queuing URL: #{url}")
+    else
+      logger.info("Skipping Non Eligible URL: #{url}")
     end
+  end
+
+  def increment_job_queue
+    job_queue.increment!(:duplicate_reference)
+    logger.info("Incrementing duplicate reference for URL: #{url}")
+  end
+
+  def eligible_to_harvest?(url)
+    return true if self.class::INCLUDE_NON_MATCHING_DOMAINS
+
+    URLValidator.nutch_request_domain?(nutch_request.domain, url)
   end
 end
