494146a9498fd6e71173aba41d55535cf2a0b88b
Sathish <reachtosathish.b@gmail.com>
2020-12-24 05:59:04 +0530

    manage consumer workers & producers

---
+++ lib/jobs/manager.rb
@@ -0,0 +1,49 @@
+module Jobs
+  # Kick starts, Manages the Q for producer and consumer
+  # Implements a Producer:Consumer Pattern
+  class Manager
+    include Log
+    MAX_Q_SIZE = 20
+    attr_reader :queue, :workers, :pool_size
+
+    def initialize(pool_size = 5)
+      @queue = SizedQueue.new(self.class::MAX_Q_SIZE)
+      @pool_size = pool_size
+    end
+
+    # @param elements [Array[JobQueue], JobQueue] adds JobQueues to a thread safe Queue
+    def queue=(elements)
+      return unless elements
+
+      @queue = Queue.new if @queue.closed?
+
+      @queue.push(elements) unless elements.respond_to?(:each)
+
+      elements.each do |element|
+        @queue.push(element)
+      end
+    end
+
+    # @param job_queue [ActiveRecord::Relation, ActiveRecord::Associations::CollectionProxy, JobQueue]
+    #   JobQueue and JobQueue AR collection object with custom scopes that responds to AR .reload method
+    # @param [&block] the block that needs to be executed on the given job queue
+    # Process the job_queue with the give block
+    def process(job_queue, &block)
+      create_workers_for(&block)
+      activate_producer_for(job_queue)
+      workers.map(&:join)
+    end
+
+    private
+
+    def create_workers_for(&block)
+      @workers = (1..pool_size).map do
+        Worker.new(self).call(&block)
+      end
+    end
+
+    def activate_producer_for(job_queue)
+      Producer.new(job_queue, self, batch_size: pool_size).call
+    end
+  end
+end
