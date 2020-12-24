73cd376f000d046099f20bdd671b2311c1d068ee
Sathish <reachtosathish.b@gmail.com>
2020-12-24 05:51:17 +0530

    Trigger the framework using Nutch Service

---
+++ app/services/nutch_service.rb
@@ -0,0 +1,26 @@
+# Trigger the Nutch Service, delegate tasks to Job Manager
+class NutchService < ApplicationService
+  ROOT_DEPTH = 0
+
+  def initialize(root_url)
+    validate_url(root_url)
+    @nutch_request = NutchRequest.create(url: root_url)
+    @job_manager = Jobs::Manager.new
+    JobQueueService.call(root_url, nutch_request.id, ROOT_DEPTH)
+  end
+
+  def execute
+    nutch_request.track_status(raise_exception: true) do
+      job_queue = JobQueue.queued.where(nutch_request: nutch_request)
+      job_manager.process(job_queue) { |job| SiteMapCatalogBuilder.perform(job.id) }
+    end
+  end
+
+  private
+
+  attr_reader :nutch_request, :job_manager
+
+  def validate_url(url)
+    raise StandardError, "invalid URL: #{url} " unless URLValidator.url?(url)
+  end
+end
