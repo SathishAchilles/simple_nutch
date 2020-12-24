24a6b1fa8d6997d18e37e4d6e64b19b35231a5de
Sathish <reachtosathish.b@gmail.com>
2020-12-24 07:14:43 +0530

    remove warn logger

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -8,5 +8,4 @@ db_config = YAML.load_file("#{Bundler.root}/config/database.yaml")
 env = ENV['SIMPLE_NUTCH_ENV'] || 'development'
 
 ActiveRecord::Base.logger = Logger.new($stdout)
-# ActiveRecord::Base.logger.level = Logger::WARN
 ActiveRecord::Base.establish_connection(db_config[env])
