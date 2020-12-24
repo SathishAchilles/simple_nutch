9c4bedaceab7a342655134cb81418c853247fc51
Sathish <reachtosathish.b@gmail.com>
2020-12-24 05:52:45 +0530

    slim down responsibility of simple_html_href_parser

--- app/services/simple_html_href_parser.rb
+++ app/services/simple_html_href_parser.rb
@@ -15,7 +15,7 @@ class SimpleHTMLHREFParser < ApplicationService
 
   # @return [Array[Array[String, Hash]]] Array of Array of URL string and mapping query params
   def execute
-    filter_attr_values.map { |value| handle_value(value) }.compact
+    filter_href_values
   end
 
   private
@@ -26,36 +26,7 @@ class SimpleHTMLHREFParser < ApplicationService
     html_nodes.css(tag).map { |a| a.attributes[tag_attr_map[tag]].try(:value) }.compact
   end
 
-  def handle_value(value)
-    if url?(value)
-      job_queue = JobQueue.find_by(nutch_request_id: job.nutch_request_id, url: value)
-      if job_queue
-        logger.info("Skipping duplicate URL: #{value}")
-        # check if sitemap exists and increment for reference count
-        job_queue&.increment!(:duplicate_reference)
-        logger.info("Incrementing duplicate reference for URL: #{value}")
-      else
-        JobQueue.create(nutch_request_id: job.nutch_request_id, url: value, depth: job.depth + 1)
-        logger.info("Queuing URL: #{value}")
-      end
-    else
-      logger.info("Skipping possible static URL #{value}")
-      nil
-    end
-  end
-
-  def handle_url(url)
-    uri = Addressable::URI.parse(url)
-    url_query_param_map = [uri.query_values]
-    uri.query_values = nil # to remove the existing query params
-    url_query_param_map.unshift(uri.to_s)
-  end
-
-  def url?(url)
-    uri = URI.parse(url)
-    uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
-  rescue URI::InvalidURIError => e
-    logger.warn(e)
-    false
+  def filter_href_values
+    html_nodes.css(tag).map { |node| node.attributes[tag_attr_map[tag]].try(:value) }.compact
   end
 end
