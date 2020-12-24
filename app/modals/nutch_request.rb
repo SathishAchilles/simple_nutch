fa8749ab1a87f99f33a5240ef3ca783801c4355a
Sathish <reachtosathish.b@gmail.com>
2020-12-24 09:15:55 +0530

    parse domain from NutchRequest

--- app/modals/nutch_request.rb
+++ app/modals/nutch_request.rb
@@ -1,7 +1,12 @@
+require 'addressable'
 # Holds the Request Details for a run
 class NutchRequest < ApplicationRecord
   include StatusTrackable
   has_many :job_queues
   has_many :site_maps
   enum status: %i[pending in_progress completed failed]
+
+  def domain
+    Addressable::URI.parse(url).domain
+  end
 end
