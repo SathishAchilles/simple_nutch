2dfb14c4bb976655b5e7050e63a3259225eab202
Sathish <reachtosathish.b@gmail.com>
2020-12-24 06:35:19 +0530

    support worker class via dependency injection

--- lib/jobs/worker.rb
+++ lib/jobs/worker.rb
@@ -2,16 +2,18 @@ module Jobs
   # Consumer Worker that consume the job and executes the given block on that data
   class Worker
     include Log
-    attr_reader :job_manager
+    attr_reader :job_manager, :worker_class
 
-    def initialize(job_manager)
+    def initialize(job_manager, worker_class)
       @job_manager = job_manager
+      @worker_class = worker_class
     end
 
     def call(&block)
       Thread.new do
         while (job = job_manager.queue.deq)
-          block.call(job)
+          worker_class.perform(job.id)
+          block.call(job) if block_given?
         end
       rescue ThreadError => exception
         logger.fatal(exception.backtrace)
